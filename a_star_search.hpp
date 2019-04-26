16870838d5681521f7573a1836c9dc41dd69d11b
Chris Schadl <cschadl@gmail.com>
2019-04-26 16:56:04 -0500

    Compiles, but returns really really bad paths

--- a_star_search.hpp
+++ a_star_search.hpp
@@ -9,6 +9,9 @@
 #include <algorithm>
 #include <limits>
 #include <functional>
+#include <queue>
+#include <optional>
+#include <utility>
 
 template <typename T, typename Val, typename Compare>
 Val get_node_score_(const T& n, std::map<T, Val, Compare>& node_score_map)
@@ -30,6 +33,48 @@ struct cost_fn_traits_
 	static constexpr value max() { return std::numeric_limits<value>::max(); }
 };
 
+enum class NodeSetType
+{
+	OPEN,
+	CLOSED
+};
+
+template <typename NodeType, typename Compare>
+struct node_info
+{
+	NodeSetType type;
+	double cost_to_node;
+	typename std::map<NodeType, node_info<NodeType, Compare>, Compare>::iterator desc;
+
+	node_info() = delete;
+
+	node_info(NodeSetType type, double cost_to_node)
+	: type(type)
+	, cost_to_node(cost_to_node)
+	{
+
+	}
+};
+
+template <typename NodeType, typename CostFn, typename Compare>
+struct node_goal_cost_estimate
+{
+	using index_t = typename std::map<NodeType, node_info<NodeType, Compare>, Compare>::iterator;
+
+	index_t	node_index;
+	typename cost_fn_traits_<CostFn, NodeType>::value cost;
+
+	bool operator<(node_goal_cost_estimate const& rhs) const
+	{
+		//if (cost != rhs.cost)
+			return cost < rhs.cost;
+
+		//return node_index < rhs.node_index;
+	}
+};
+
+
+
 /// Implicit graph A* search
 /// @return The shortest path from the start node to the goal node
 ///			if one exists, otherwise, return an empty list.
@@ -47,44 +92,28 @@ std::list<NodeType> a_star_search(
 	typename cost_fn_traits_<CostFn, NodeType>::value max_cost = cost_fn_traits_<CostFn, NodeType>::max())
 {
 	using cost_fn_t = 			typename cost_fn_traits_<CostFn, NodeType>::value;
-	using node_map_t =		 	std::map<NodeType, NodeType, Compare>;
-	using path_score_map_t = 	std::map<NodeType, cost_fn_t, Compare>;
-	using node_set_t = 			std::set<NodeType, Compare>;
 
-	node_map_t	descendants;	// For each node, which node it can be most efficiently
-										// reached from.  If a node can be reached from many nodes,
-										// m_descendants will eventually contain the most efficient
-										// previous step.
-										// Used for path reconstruction
+	using node_goal_cost_est_t = node_goal_cost_estimate<NodeType, CostFn, Compare>;
+	using fringe_pq_t = std::priority_queue<node_goal_cost_est_t>;
+	using node_info_t = node_info<NodeType, Compare>;
+	using node_collection_t = std::map<NodeType, node_info_t, Compare>;
 																
-	node_set_t closed_set;
-
-	node_set_t open_set;
-	open_set.insert(start_node);
-
-	// Total cost of getting from the start node to a given node
-	path_score_map_t g_score;
-	g_score[start_node] = 0;
-
-	// Total cost estimate of getting from the start node to the
-	// goal node by taking a path that passes throught a given node
-	path_score_map_t f_score;
-	f_score[start_node] = cost_fn(start_node, goal_node);
+	fringe_pq_t fringe;
+	node_collection_t nodes;
+	{
+		typename node_collection_t::iterator start_node_it;
+		tie(start_node_it, std::ignore) = nodes.emplace(std::make_pair(start_node, node_info_t(NodeSetType::OPEN, 0.0)));
+		fringe.emplace(node_goal_cost_est_t{start_node_it, cost_fn(start_node, goal_node)});
+	}
 
 	std::list<NodeType> path;
 
-	while (!open_set.empty())
+	while (!fringe.empty())
 	{
-		// Get the node in the open set having the lowest f_score value
-		// It would be good to use a bimap for this, but
-		// for now, we'll just do a linear search
-		auto min_f_score_node = std::min_element(open_set.begin(), open_set.end(),
-			[&f_score](const NodeType& n1, const NodeType& n2)
-			{
-				return get_node_score_(n1, f_score) < get_node_score_(n2, f_score);
-			});
+		auto min_cost_node = fringe.top();
+		fringe.pop();
 
-		NodeType n = *min_f_score_node;
+		NodeType n = min_cost_node.node_index->first;
 
 		if (n == goal_node)
 		{
@@ -93,43 +122,67 @@ std::list<NodeType> a_star_search(
 			// Reconstruct path and return
 			while (n != start_node)
 			{
-				n = descendants[n];
+				auto n_it = nodes.find(n)->second;
+				auto desc_it = n_it.desc;
+
+				n = desc_it->first;
+
 				path.push_front(n);
 			}
 			
 			return path;
 		}
 
-		open_set.erase(n);
-		closed_set.insert(n);
+		auto n_it = nodes.find(n);
+		node_info_t& n_info = n_it->second;
+		n_info.type = NodeSetType::CLOSED;
 
 		auto neighbors = expand_fn(n);
 		for (auto adj_node : neighbors)
 		{
-			if (closed_set.find(adj_node) != closed_set.end())
+			auto adj_node_it = nodes.find(adj_node);
+			if (adj_node_it != nodes.end() && adj_node_it->second.type == NodeSetType::CLOSED)
 			{
 				// Neighbor already evaluated
 				continue;
 			}
 
 			// Distance from the starting node to a neighbor
-			cost_fn_t const tentative_g_score = get_node_score_(n, g_score) + neighbor_weight_fn(n, adj_node);
+			cost_fn_t const tentative_g_score = n_info.cost_to_node + neighbor_weight_fn(n, adj_node);
 			cost_fn_t const f_score_ = tentative_g_score + cost_fn(adj_node, goal_node);
 
 			if (f_score_ > max_cost)
 				break; // We won't find a better solution
 
-			if (open_set.find(adj_node) == open_set.end())
-				open_set.insert(adj_node);	// Discover a new node
-			else if (tentative_g_score >= get_node_score_(adj_node, g_score))
-				continue; // Sub-optimal path
+			if (adj_node_it == nodes.end())
+			{
+				// discover a new node
+				tie(adj_node_it, std::ignore) = nodes.emplace(std::make_pair(adj_node, node_info_t(NodeSetType::OPEN, tentative_g_score)));
+			}
+			else if (tentative_g_score >= adj_node_it->second.cost_to_node)
+				continue;	// Sub-optimal path
 
-			descendants[adj_node] = n;
-			g_score[adj_node] = tentative_g_score;
-			f_score[adj_node] = f_score_;
+			adj_node_it->second.desc = n_it;
+
+			fringe.emplace(node_goal_cost_est_t{adj_node_it, f_score_});
 		}
 	}
 
 	// No path exists
 	return path;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
