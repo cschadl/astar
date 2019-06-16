66563d7b1ff2c8b543b4d1b4ba9f60f023fd8b0d
Chris Schadl <cschadl@gmail.com>
2019-06-16 17:58:09 -0500

    canonical magic squares

--- solve_magic_square.cpp
+++ solve_magic_square.cpp
@@ -16,6 +16,22 @@ using namespace cds::astar;
 
 using magic_square_t = std::vector< std::vector<int> >;
 
+namespace
+{
+
+static std::vector<magic_square_t> canonical_magic_square = {
+		{ { 8, 1, 6 }, { 3, 5, 7 }, { 4, 9, 2 } },
+		{ { 6, 1, 8 }, { 7, 5, 3 }, { 2, 9, 4 } },
+		{ { 4, 9, 2 }, { 3, 5, 7 }, { 8, 1, 6 } },
+		{ { 2, 5, 4 }, { 7, 5, 3 }, { 6, 1, 8 } },
+		{ { 8, 3, 4 }, { 1, 5, 9 }, { 6, 7, 2 } },
+		{ { 4, 3, 8 }, { 9, 5, 1 }, { 2, 7, 6 } },
+		{ { 6, 7, 2 }, { 1, 5, 9 }, { 8, 3, 4 } },
+		{ { 2, 7, 6 }, { 9, 5, 1 }, { 4, 3, 8 } }
+};
+
+}
+
 // expand
 vector<magic_square_t> expand(magic_square_t const& sq)
 {
