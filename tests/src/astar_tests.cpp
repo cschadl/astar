c43b271510c8255cc432464ffb4eabbdd98f774a
Chris Schadl <cschadl@gmail.com>
2024-05-22 22:29:33 -0500

    Make typed test suite for n_sq_puzzle tests

--- tests/src/astar_tests.cpp
+++ tests/src/astar_tests.cpp
@@ -5,8 +5,6 @@
 
 #include <astar/a_star_search.hpp>
 #include <astar/ida_star_search.hpp>
-#include <n_sq_puzzle.hpp>
-#include <solve_helpers.hpp>
 
 #include <algorithm>
 #include <functional>
@@ -150,51 +148,4 @@ TYPED_TEST(DijkstraGraphSearchTest, DijkstraGraphSearchImplementations)
 
 	EXPECT_EQ(computed_path_cost, 17);
 	EXPECT_EQ(path_cost, computed_path_cost);
-}
-
-TEST(AStarSearch, NSqPuzzle)
-{
-	n_sq_puzzle<3> puzzle;
-	puzzle.set({7, 2, 4, 3, 0, 1, 8, 5, 6});
-
-	n_sq_puzzle<3> goal_state;
-	auto is_goal = [&goal_state](n_sq_puzzle<3> const& n) { return n == goal_state; };
-
-	auto heuristic_fn = [&goal_state](n_sq_puzzle<3> const& n) { return tile_taxicab_dist(n, goal_state); };
-
-	auto dist_fn = [](n_sq_puzzle<3> const&, n_sq_puzzle<3> const&) { return 1; };
-
-	std::vector<n_sq_puzzle<3>> path;
-
-	bool found_path = astar::a_star_search(
-		puzzle, &expand<3>, heuristic_fn, dist_fn, is_goal,
-		std::back_inserter(path));
-
-	ASSERT_TRUE(found_path);
-
-	using state_t = n_sq_puzzle<3>::state_t;
-
-	std::vector<n_sq_puzzle<3>> expected_path;
-	add_puzzle_state<3>(expected_path, { 7, 2, 4, 3, 0, 1, 8, 5, 6 });
-	add_puzzle_state<3>(expected_path, { 7, 0, 4, 3, 2, 1, 8, 5, 6 });
-	add_puzzle_state<3>(expected_path, { 7, 4, 0, 3, 2, 1, 8, 5, 6 });
-	add_puzzle_state<3>(expected_path, { 7, 4, 1, 3, 2, 0, 8, 5, 6 });
-	add_puzzle_state<3>(expected_path, { 7, 4, 1, 3, 0, 2, 8, 5, 6 });
-	add_puzzle_state<3>(expected_path, { 7, 4, 1, 0, 3, 2, 8, 5, 6 });
-	add_puzzle_state<3>(expected_path, { 0, 4, 1, 7, 3, 2, 8, 5, 6 });
-	add_puzzle_state<3>(expected_path, { 4, 0, 1, 7, 3, 2, 8, 5, 6 });
-	add_puzzle_state<3>(expected_path, { 4, 1, 0, 7, 3, 2, 8, 5, 6 });
-	add_puzzle_state<3>(expected_path, { 4, 1, 2, 7, 3, 0, 8, 5, 6 });
-	add_puzzle_state<3>(expected_path, { 4, 1, 2, 7, 0, 3, 8, 5, 6 });
-	add_puzzle_state<3>(expected_path, { 4, 1, 2, 7, 5, 3, 8, 0, 6 });
-	add_puzzle_state<3>(expected_path, { 4, 1, 2, 7, 5, 3, 0, 8, 6 });
-	add_puzzle_state<3>(expected_path, { 4, 1, 2, 0, 5, 3, 7, 8, 6 });
-	add_puzzle_state<3>(expected_path, { 0, 1, 2, 4, 5, 3, 7, 8, 6 });
-	add_puzzle_state<3>(expected_path, { 1, 0, 2, 4, 5, 3, 7, 8, 6 });
-	add_puzzle_state<3>(expected_path, { 1, 2, 0, 4, 5, 3, 7, 8, 6 });
-	add_puzzle_state<3>(expected_path, { 1, 2, 3, 4, 5, 0, 7, 8, 6 });
-	add_puzzle_state<3>(expected_path, { 1, 2, 3, 4, 5, 6, 7, 8, 0 });
-
-	EXPECT_FALSE(path.empty());
-	EXPECT_EQ(path, expected_path);
 }
