43cc18b2596ad9fbe5f8292dc045f57efac8f3ea
Chris Schadl <cschadl@gmail.com>
2024-05-25 13:25:26 -0500

    move stuff from grid.h to shortest_path_tests.cpp

--- tests/src/shortest_path_tests.cpp
+++ tests/src/shortest_path_tests.cpp
@@ -6,11 +6,45 @@
 #include <vector>
 #include <unordered_set>
 #include <limits>
+#include <cmath>
 
-#include "grid.h"
 #include "get_path_cost.h"
 
 using namespace cds;
+
+namespace
+{
+	struct grid_node
+	{
+		int x{0};
+		int y{0};
+
+		bool operator==(grid_node const& n) const { return this->x == n.x && this->y == n.y; }
+		bool operator!=(grid_node const& n) const { return !(*this == n); }
+	};
+
+}
+
+namespace std
+{
+
+template <>
+class hash<grid_node>
+{
+public:
+	size_t operator()(const grid_node& node) const
+	{
+		constexpr unsigned int p1 = 73856093;
+		constexpr unsigned int p2 = 83492791;
+
+		int xi = std::floor(node.x);
+		int yi = std::floor(node.y);
+
+		return (xi * p1) ^ (yi * p2);
+	}
+};
+
+}
 
 namespace
 {
