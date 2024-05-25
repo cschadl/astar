8370adabf7f7a3946cd15945fe98585f625b320b
Chris Schadl <cschadl@gmail.com>
2024-05-25 13:20:13 -0500

    move stuff from adj_list_graph.h to dijkstra_search_tests.cpp

--- tests/src/dijkstra_search_tests.cpp
+++ tests/src/dijkstra_search_tests.cpp
@@ -1,6 +1,5 @@
 #include <gtest/gtest.h>
 
-#include "adj_list_graph.h"
 #include "get_path_cost.h"
 
 #include <astar/a_star_search.hpp>
@@ -23,6 +22,34 @@ namespace
 	bool is_goal(char n)
 	{
 		return n == 'z';
+	}
+
+	using neighbor_weight_t = std::pair<char, int>;
+	using adj_list_graph_t = std::map<char, std::map<char, int>>;
+
+	std::vector<char> expand_adj_list_graph(adj_list_graph_t const& graph, char node)
+	{
+		auto n_it = graph.find(node);
+		if (n_it == graph.end())
+			return std::vector<char>();
+
+		auto const& neighbors = n_it->second;
+		if (neighbors.empty())
+			return std::vector<char>();
+
+		std::vector<char> neighbor_nodes(neighbors.size());
+		std::transform(neighbors.begin(), neighbors.end(),
+			neighbor_nodes.begin(),
+			[](neighbor_weight_t const& nw) { return nw.first; });
+
+		return neighbor_nodes;
+	}
+
+	int neighbor_weight(adj_list_graph_t const& graph, char n, char m)
+	{
+		// does not check that n, m are in graph (we'll throw an exception)
+		auto const& n_neighbors_weights = graph.at(n);
+		return n_neighbors_weights.at(m);
 	}
 
 	adj_list_graph_t const theGraph = {
